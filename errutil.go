f3ac120bde96c3df8c96f18fe8cc8dda5e0e1c0c
LEGOlord208 <LEGOlord208@gmail.com>
2017-03-31 20:31:49 +0200

    Go formatting :(

--- errutil.go
+++ errutil.go
@@ -1,59 +1,59 @@
-package stdutil;
+package stdutil
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"runtime/debug"
-	"io"
 )
 
 var (
 	// Set whether or not PrintErr() should print a stack trace.
-	ShouldTrace bool;
+	ShouldTrace bool
 
 	// Sets the output PrintErr() should use.
 	// Defaults to os.Stderr
-	ErrOutput io.Writer = os.Stderr;
+	ErrOutput io.Writer = os.Stderr
 
 	// A slice of functions (events) to call before PrintErr() is called.
 	// The final error message that will be printed out it sent as argument.
 	// If returned true, the function is cancelled completely.
 	// Parameters: full message (message + error), message, error.
-	EventPrePrintError []func(string, string, error) bool;
+	EventPrePrintError []func(string, string, error) bool
 
 	// A slice of functions (events) to call after PrintErr() is called.
 	// The final error message that will be printed out it sent as argument.
 	// Parameters: full message (message + error), message, error.
-	EventPostPrintError []func(string, string, error);
+	EventPostPrintError []func(string, string, error)
 )
 
 // Prints an error message.
 // Any argument may be zero.
-func PrintErr(msg string, err error){
-	var text string;
-	if(err == nil){
-		text = msg;
-	} else if(msg == ""){
-		text = "An error occured: " + err.Error();
+func PrintErr(msg string, err error) {
+	var text string
+	if err == nil {
+		text = msg
+	} else if msg == "" {
+		text = "An error occured: " + err.Error()
 	} else {
-		text = msg + ": " + err.Error();
+		text = msg + ": " + err.Error()
 	}
 
-	defer func(){
-		for _, event := range EventPostPrintError{
-			event(text, msg, err);
+	defer func() {
+		for _, event := range EventPostPrintError {
+			event(text, msg, err)
 		}
-	}();
+	}()
 
-	for _, event := range EventPrePrintError{
-		if(event(text, msg, err)){
-			return;
+	for _, event := range EventPrePrintError {
+		if event(text, msg, err) {
+			return
 		}
 	}
 
-	fmt.Fprintln(ErrOutput, text);
+	fmt.Fprintln(ErrOutput, text)
 
-	if(ShouldTrace){
-		debug.PrintStack();
+	if ShouldTrace {
+		debug.PrintStack()
 	}
 }
--- stdin.go
+++ stdin.go
@@ -1,61 +1,61 @@
-package stdutil;
+package stdutil
 
 import (
 	"bufio"
-	"os"
 	"io"
+	"os"
 	"strings"
 )
 
-var scanner *bufio.Scanner;
+var scanner *bufio.Scanner
 
 // Scan a line.
 // Return any errors.
-func Scanln() (string, error){
-	if(scanner == nil){
-		scanner = bufio.NewScanner(os.Stdin);
+func Scanln() (string, error) {
+	if scanner == nil {
+		scanner = bufio.NewScanner(os.Stdin)
 	}
 
-	if(scanner.Scan()){
-		return scanner.Text(), nil;
+	if scanner.Scan() {
+		return scanner.Text(), nil
 	} else {
-		err := scanner.Err();
-		if(err == nil){
-			return "", io.EOF;
+		err := scanner.Err()
+		if err == nil {
+			return "", io.EOF
 		} else {
-			PrintErr("Could not read", err);
-			return "", err;
+			PrintErr("Could not read", err)
+			return "", err
 		}
 	}
 }
 
 // Scan a line, but trim the result.
 // Return any errors.
-func ScanTrim() (string, error){
-	str, err := Scanln();
-	return strings.TrimSpace(str), err;
+func ScanTrim() (string, error) {
+	str, err := Scanln()
+	return strings.TrimSpace(str), err
 }
 
 // Scan a line.
 // If failed, exit the program.
-func MustScanln() string{
-	in, err := Scanln();
-	if(err != nil){
-		os.Exit(1);
-		return "";
+func MustScanln() string {
+	in, err := Scanln()
+	if err != nil {
+		os.Exit(1)
+		return ""
 	} else {
-		return in;
+		return in
 	}
 }
 
 // Scan a line, but trim the result.
 // If failed, exit the program.
-func MustScanTrim() string{
-	return strings.TrimSpace(MustScanln());
+func MustScanTrim() string {
+	return strings.TrimSpace(MustScanln())
 }
 
 // Scan a line, but trim and make the result lowercase.
 // If failed, exit the program.
-func MustScanLower() string{
-	return strings.ToLower(MustScanTrim());
+func MustScanLower() string {
+	return strings.ToLower(MustScanTrim())
 }
